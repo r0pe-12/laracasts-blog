b80252955c286963c161264d5423377f6f06840a
Petar Simonovic <simonovicp12@gmail.com>
2022-06-29 14:07:00 +0200

    search working but not perfect

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if($posts)
+        @if($posts->count())
             <x-posts-grid :posts="$posts"/>
         @else
             <p class="text-center">No posts yet. Check back later.</p>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::with(['author', 'category'])->latest();
+    if (request('search')){
+        $posts = $posts->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
+
     return view('posts',[
-        'posts' => Post::with(['author', 'category'])->latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all(),
     ]);
 })->name('home');
